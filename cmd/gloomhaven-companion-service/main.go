80271defaf37f4d6327135878e717b7988dc6483
Zane-T-Rice <zanetrice@gmail.com>
2025-10-26 06:35:26 -0400

    Cleaning up code a bit.

--- cmd/gloomhaven-companion-service/main.go
+++ cmd/gloomhaven-companion-service/main.go
@@ -3,8 +3,8 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	ensurevalidtoken "gloomhaven-companion-service/internal/ensure-valid-token"
+	setenvironmentvariables "gloomhaven-companion-service/internal/set-environment-variables"
 	"log"
 	"maps"
 	"os"
@@ -13,13 +13,9 @@ import (
 	"github.com/auth0/go-jwt-middleware/v2/validator"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
 	fiberadapter "github.com/awslabs/aws-lambda-go-api-proxy/fiber"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/adaptor" // or v3
-	"github.com/joho/godotenv"
 	"github.com/valyala/fasthttp"
 )
 
@@ -28,52 +24,7 @@ var fiberLambda *fiberadapter.FiberLambda
 
 // init the Fiber Server
 func init() {
-	godotenv.Load()
-	localServicePort := os.Getenv("LOCAL_SERVICE_PORT")
-	if localServicePort == "" {
-		// Load any environment variables from AWS Secret Manager
-		audienceSecretName := "gloomhaven-companion-service-audience"
-		issuerSecretName := "gloomhaven-companion-service-issuer"
-		region := "us-east-1"
-
-		config, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(region))
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		// Create Secrets Manager client
-		svc := secretsmanager.NewFromConfig(config)
-
-		input := &secretsmanager.GetSecretValueInput{
-			SecretId:     aws.String(audienceSecretName),
-			VersionStage: aws.String("AWSCURRENT"), // VersionStage defaults to AWSCURRENT if unspecified
-		}
-		result, err := svc.GetSecretValue(context.TODO(), input)
-		if err != nil {
-			log.Fatal(err.Error())
-		}
-		type Secret struct {
-			Secret string `json:"value"`
-		}
-		audienceSecret := Secret{}
-		json.Unmarshal([]byte(*result.SecretString), &audienceSecret)
-
-		input = &secretsmanager.GetSecretValueInput{
-			SecretId:     aws.String(issuerSecretName),
-			VersionStage: aws.String("AWSCURRENT"), // VersionStage defaults to AWSCURRENT if unspecified
-		}
-		result, err = svc.GetSecretValue(context.TODO(), input)
-		if err != nil {
-			log.Fatal(err.Error())
-		}
-
-		issuerSecret := Secret{}
-		json.Unmarshal([]byte(*result.SecretString), &issuerSecret)
-
-		log.Println("Setting the AUDIENCE and ISSUER")
-		os.Setenv("AUDIENCE", audienceSecret.Secret)
-		os.Setenv("ISSUER", issuerSecret.Secret)
-	}
+	setenvironmentvariables.SetEnvironmentVariables()
 
 	app = fiber.New()
 
---
+++ internal/set-environment-variables/set-environment-variables.go
@@ -0,0 +1,50 @@
+package setenvironmentvariables
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
+	"github.com/joho/godotenv"
+)
+
+type Secret struct {
+	Secret string `json:"value"`
+}
+
+func SetEnvironmentVariables() {
+	log.Println("IN SetEnvironmentVariables")
+	godotenv.Load()
+	localServicePort := os.Getenv("LOCAL_SERVICE_PORT")
+	if localServicePort == "" {
+		config, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-east-1"))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		secretManagerClient := secretsmanager.NewFromConfig(config)
+
+		setEnvironmentVariable("gloomhaven-companion-service-audience", "AUDIENCE", secretManagerClient)
+		setEnvironmentVariable("gloomhaven-companion-service-issuer", "ISSUER", secretManagerClient)
+	}
+}
+
+func setEnvironmentVariable(secretName string, environmentVariableName string, svc *secretsmanager.Client) {
+	log.Println("IN setEnvironmentVariable")
+	input := &secretsmanager.GetSecretValueInput{
+		SecretId:     aws.String(secretName),
+		VersionStage: aws.String("AWSCURRENT"), // VersionStage defaults to AWSCURRENT if unspecified
+	}
+	result, err := svc.GetSecretValue(context.TODO(), input)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	audienceSecret := Secret{}
+	json.Unmarshal([]byte(*result.SecretString), &audienceSecret)
+	os.Setenv(environmentVariableName, audienceSecret.Secret)
+}
