85932af1d4d5653538c258eaaca8130ecf95fffd
Zane-T-Rice <zanetrice@gmail.com>
2025-11-17 11:10:24 -0500

    Adding scenario level to support templates.

--- internal/dto/scenario.go
+++ internal/dto/scenario.go
@@ -3,17 +3,19 @@ package dto
 import "gloomhaven-companion-service/internal/types"
 
 type Scenario struct {
-	Parent string  `dynamodbav:"parent" json:"parent"`
-	Entity string  `dynamodbav:"entity" json:"entity"`
-	Name   *string `dynamodbav:"name" json:"name"`
-	Groups *string `dynamodbav:"groups,omitempty" json:"groups"`
+	Parent        string  `dynamodbav:"parent" json:"parent"`
+	Entity        string  `dynamodbav:"entity" json:"entity"`
+	Name          *string `dynamodbav:"name" json:"name"`
+	Groups        *string `dynamodbav:"groups,omitempty" json:"groups"`
+	ScenarioLevel *int    `dynamodbav:"scenario_level,omitempty" json:"scenarioLevel"`
 }
 
 func NewScenario(item types.ScenarioItem) Scenario {
 	return Scenario{
-		Parent: item.Parent,
-		Entity: item.Entity,
-		Name:   item.Name,
-		Groups: item.Groups,
+		Parent:        item.Parent,
+		Entity:        item.Entity,
+		Name:          item.Name,
+		Groups:        item.Groups,
+		ScenarioLevel: item.ScenarioLevel,
 	}
 }
--- internal/types/scenario.go
+++ internal/types/scenario.go
@@ -1,17 +1,20 @@
 package types
 
 type ScenarioCreateInput struct {
-	Name   *string `dynamodbav:"name,omitempty" json:"name"`
-	Groups *string `dynamodbav:"groups,omitempty" json:"groups"`
+	Name          *string `dynamodbav:"name,omitempty" json:"name"`
+	Groups        *string `dynamodbav:"groups,omitempty" json:"groups"`
+	ScenarioLevel *int    `dynamodbav:"scenario_level,omitempty" json:"scenarioLevel"`
 }
 
 type ScenarioPatchInput struct {
-	Name   *string `dynamodbav:"name,omitempty" json:"name"`
-	Groups *string `dynamodbav:"groups,omitempty" json:"groups"`
+	Name          *string `dynamodbav:"name,omitempty" json:"name"`
+	Groups        *string `dynamodbav:"groups,omitempty" json:"groups"`
+	ScenarioLevel *int    `dynamodbav:"scenario_level,omitempty" json:"scenarioLevel"`
 }
 
 type ScenarioItem struct {
-	Item   `dynamodbav:",inline"`
-	Name   *string `dynamodbav:"name,omitempty" json:"name"`
-	Groups *string `dynamodbav:"groups,omitempty" json:"groups"`
+	Item          `dynamodbav:",inline"`
+	Name          *string `dynamodbav:"name,omitempty" json:"name"`
+	Groups        *string `dynamodbav:"groups,omitempty" json:"groups"`
+	ScenarioLevel *int    `dynamodbav:"scenario_level,omitempty" json:"scenarioLevel"`
 }
