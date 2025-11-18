e33eef6238f3e3758bd61f88249bce1befd861eb
Zane-T-Rice <zanetrice@gmail.com>
2025-11-18 11:10:53 -0500

    Adding seeding of templates to seed_dynamodb.sh.

---
+++ scripts/all_local.sh
@@ -0,0 +1,10 @@
+TERMINAL=alacritty
+$TERMINAL -e scripts/build.sh &
+$TERMINAL -e scripts/start_dynamodb_local.sh &
+$TERMINAL -e scripts/local_websocket.sh &
+$TERMINAL -e ./dist/gloomhaven-companion-service/bootstrap &
+
+# This one is tricky, but for now just using a sleep. It can always be re-run
+# if the dynamodb local does not come up in time.
+sleep 5
+$TERMINAL -e scripts/seed_local_dynamodb.sh &
--- scripts/seed_dynamodb.sh
+++ scripts/seed_dynamodb.sh
@@ -1,4 +1,6 @@
-aws dynamodb create-table \
+source ./.env
+
+AWS_PAGER="" aws dynamodb create-table \
     --table-name gloomhaven-companion-service \
     --attribute-definitions \
         AttributeName=parent,AttributeType=S \
@@ -18,3 +20,21 @@ aws dynamodb create-table \
                 }
             }
         ]"
+
+TOKEN_RESPONSE=`curl --request POST \
+  --url "$ISSUER"oauth/token \
+  --header 'Content-Type: application/x-www-form-urlencoded' \
+  --data grant_type=client_credentials \
+  --data client_id=$CLIENT_ID \
+  --data client_secret=$CLIENT_SECRET \
+  --data audience=$AUDIENCE`;
+
+ACCESS_TOKEN=`echo $TOKEN_RESPONSE | grep -Eo '"access_token"[^,]*' | grep -Eo '[^:]*$' | tr -d '"'`;
+
+for i in $(ls templates/*); do \
+  curl --request POST \
+    --url $GLOOMHAVEN_COMPANION_SERVICE_URL/templates \
+    --header "Authorization: Bearer $ACCESS_TOKEN" \
+    --header 'Content-Type: application/json' \
+    --data "@$i";
+done
